7b6a17d6542315b58e5cdc804b57c05788670afc
thpugnat <thomas.pugnat@cern.ch>
2024-08-08 22:20:27 +0200

    same as before

--- xdyna/__init__.py
+++ xdyna/__init__.py
@@ -5,7 +5,7 @@
 
 from .general import _pkg_root, __version__
 
-from .plot import plot_particles, plot_da_border, plot_da_border, plot_davsturns_border
+from .plot import plot_particles, plot_border, plot_davsturns_border, plot_davsturns_extremum
 
 from .da import DA
 from .da_meta import regenerate_meta_file
